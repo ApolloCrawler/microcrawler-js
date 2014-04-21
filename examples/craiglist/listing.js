71c9741a3f050b5589d42af1cc8b3602fe2c4074
korczis <korczis@gmail.com>
2014-04-20 22:32:13 -0700

    Craiglist listing

---
+++ examples/craiglist/listing.js
@@ -0,0 +1,64 @@
+// Copyright, 2013-2014, by Tomas Korcak. <korczis@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+(function () {
+    'use strict';
+
+    var define = require('amdefine')(module);
+
+    /**
+     * Array of modules this one depends on.
+     * @type {Array}
+     */
+    var deps = [
+        'querystring',
+        'url'
+    ];
+
+    define(deps, function(querystring, url) {
+        module.exports = function($, item) {
+            var results = [];
+
+            var parsedUrl = url.parse(item.url);
+
+            $('a.button.next').each(function($) {
+                results.push({
+                    type: 'url',
+                    url: parsedUrl.protocol + '//' + parsedUrl.host + this.attr('href'),
+                    processor: 'craiglist.listing'
+                });
+            });
+
+            $('div.content > p.row > span.pl > a').each(function($) {
+                var result = {
+                    title: this.text(),
+                    url: this.attr('href')
+                };
+
+                results.push({
+                    type: 'data',
+                    data: result
+                });
+            });
+
+            return results;
+        }
+    });
+}());
