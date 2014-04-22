d45917975bcdfc6d160c7fab61ec4720b0bd839e
korczis <korczis@gmail.com>
2014-04-21 19:50:35 -0700

    Engine.enqueueUrl tests

--- test/engineTest.js
+++ test/engineTest.js
@@ -26,6 +26,21 @@
         , Mc = require('./../lib');
 
     describe('Engine', function () {
+        var testData1 = {
+            url: 'http://google.com',
+            processor: 'seznam.listing'
+        };
+
+        var testData2 = {
+            url: 'http://seznam.cz',
+            processor: 'seznam.listing'
+        };
+
+        var testData3 = {
+            url: 'http://google.com',
+            processor: 'google.details'
+        };
+
         it('Module is defined', function () {
             Mc.Engine.should.not.equal(null);
         });
@@ -35,6 +50,33 @@
             instance.should.not.equal(null);
             instance.should.be.an.instanceof(Engine);
             instance.opts.should.equal(Engine.defaultOptions);
+        });
+
+        describe('enqueueUrl()', function() {
+            it('Is defined()', function() {
+                var engine = new Engine();
+                engine.enqueueUrl.should.not.equal(null);
+            });
+
+            it('Should enqueue unique URL', function() {
+                var engine = new Engine();
+
+                engine.enqueueUrl.should.not.equal(null);
+
+                var res = engine.enqueueUrl(testData1.url, testData1.processor, null);
+                chai.expect(res).to.equal(true);
+            });
+
+            it('Should enqueue unique same URL only once', function() {
+                var engine = new Engine();
+                engine.enqueueUrl.should.not.equal(null);
+
+                var res = engine.enqueueUrl(testData1.url, testData1.processor, null);
+                chai.expect(res).to.equal(true);
+
+                res = engine.enqueueUrl(testData1.url, testData1.processor, null);
+                chai.expect(res).to.equal(false);
+            });
         });
 
         describe('isDone()', function() {
@@ -72,46 +114,21 @@
             it('Should return true for same url and same processor', function () {
                 var engine = new Engine();
 
-                var testData = {
-                    url: 'http://google.com',
-                    processor: 'google.listing'
-                };
-
-                engine.enqueueUrl(testData.url, testData.processor, null);
-                var res = engine.wasAlreadyEnqueued(testData.url, testData.processor);
+                engine.enqueueUrl(testData1.url, testData1.processor, null);
+                var res = engine.wasAlreadyEnqueued(testData1.url, testData1.processor);
                 chai.expect(res).to.equal(true);
             });
 
             it('Should return false for same url and different processor', function () {
                 var engine = new Engine();
 
-                var testData1 = {
-                    url: 'http://seznam.cz',
-                    processor: 'seznam.listing'
-                };
-
-                var testData2 = {
-                    url: 'http://seznam.cz',
-                    processor: 'seznam.details'
-                };
-
                 engine.enqueueUrl(testData1.url, testData1.processor, null);
-                var res = engine.wasAlreadyEnqueued(testData2.url, testData2.processor);
+                var res = engine.wasAlreadyEnqueued(testData3.url, testData3.processor);
                 chai.expect(res).to.equal(false);
             });
 
             it('Should return false for different url and same processor', function () {
                 var engine = new Engine();
-
-                var testData1 = {
-                    url: 'http://google.com',
-                    processor: 'seznam.listing'
-                };
-
-                var testData2 = {
-                    url: 'http://seznam.cz',
-                    processor: 'seznam.listing'
-                };
 
                 engine.enqueueUrl(testData1.url, testData1.processor, null);
                 var res = engine.wasAlreadyEnqueued(testData2.url, testData2.processor);
