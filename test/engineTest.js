fa3e86f2bd39e69afb184bb4bc734c4e56f8f399
korczis <korczis@gmail.com>
2014-04-27 18:48:34 -0700

    Move higher functions from queue to engine to keep queue as abstract as possible

--- test/engineTest.js
+++ test/engineTest.js
@@ -26,6 +26,21 @@
         , Mc = require('./../lib');
 
     describe('Engine', function () {
+        var testData1 = {
+            url: 'http://google.com',
+            processor: 'seznam.listing'
+        };
+
+        var testData2 = {
+            url: 'http://seznam.cz',
+            processor: 'seznam.listing'
+        };
+
+        var testData3 = {
+            url: 'http://google.com',
+            processor: 'google.details'
+        };
+
         it('Module is defined', function () {
             Mc.Engine.should.not.equal(null);
         });
@@ -86,5 +101,90 @@
                 instance.run();
             });
         });
+
+        describe('enqueueUrl()', function () {
+            it('Is defined()', function () {
+                var instance = new Engine();
+                instance.enqueueUrl.should.not.equal(null);
+            });
+
+            it('Should enqueue unique URL', function () {
+                var instance = new Engine();
+
+                instance.enqueueUrl.should.not.equal(null);
+
+                var res = instance.enqueueUrl(testData1);
+                chai.expect(res).to.equal(true);
+            });
+
+            it('Should enqueue unique URL together with data', function () {
+                var instance = new Engine();
+
+                instance.enqueueUrl.should.not.equal(null);
+
+                var data = {
+                    name: "John Doe"
+                };
+
+                var res = instance.enqueueUrl({
+                    url: testData1.url,
+                    processor: testData1.processor,
+                    data: data
+                });
+                chai.expect(res).to.equal(true);
+
+                chai.expect(instance.queue.get('requested').data).to.equal(data);
+            });
+
+            it('Should enqueue unique same URL only once', function () {
+                var instance = new Engine();
+                instance.enqueueUrl.should.not.equal(null);
+
+                var res = instance.enqueueUrl(testData1.url, testData1.processor, null);
+                chai.expect(res).to.equal(true);
+
+                res = instance.enqueueUrl(testData1.url, testData1.processor, null);
+                chai.expect(res).to.equal(false);
+            });
+        });
+
+        describe('isDone()', function () {
+            it('Is defined', function () {
+                var instance = new Engine();
+                instance.isDone.should.not.equal(null);
+            });
+        });
+
+        describe('wasAlreadyEnqueued()', function () {
+            it('Should be defined', function () {
+                var instance = new Engine();
+                instance.wasAlreadyEnqueued.should.not.equal(null);
+            });
+
+            it('Should return true for same url and same processor', function () {
+                var instance = new Engine();
+
+                instance.enqueueUrl(testData1);
+                var res = instance.wasAlreadyEnqueued(testData1);
+                chai.expect(res).to.equal(true);
+            });
+
+            it('Should return false for same url and different processor', function () {
+                var instance = new Engine();
+
+                instance.enqueueUrl(testData1);
+                var res = instance.wasAlreadyEnqueued(testData3);
+                chai.expect(res).to.equal(false);
+            });
+
+            it('Should return false for different url and same processor', function () {
+                var instance = new Engine();
+
+                instance.enqueueUrl(testData1);
+                var res = instance.wasAlreadyEnqueued(testData2);
+                chai.expect(res).to.equal(false);
+            });
+        });
+
     });
 }());
