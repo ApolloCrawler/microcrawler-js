795fb69e5a6d49a21c2293b378fd67ffca314785
Tomas Korcak <korczis@gmail.com>
2014-08-05 17:28:34 -0700

    Added sample parser for list of categories

---
+++ examples/czso/index.js
@@ -0,0 +1,58 @@
+// Copyright, 2013-2014, by Tomas Korcak. <korczis@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// For crawling http://vdb.czso.cz/vdb/ukazatele.jsp?typ=0
+
+(function () {
+    'use strict';
+
+    var define = require('amdefine')(module);
+
+    /**
+     * Array of modules this one depends on.
+     * @type {Array}
+     */
+    var deps = [];
+
+    define(deps, function() {
+        module.exports = function($, item) {
+            var results = [];
+
+            $('table.orient100 > tbody > tr').each(function() {
+                var first = $(this).find('td:nth-child(1)').text();
+                var second = $(this).find('td:nth-child(2)').text();
+
+                if(!first || !second) {
+                    return;
+                }
+
+                results.push({
+                    type: 'data',
+                    data: {
+                        code: first.split(/[\s]+/).join(''),
+                        title: second.split(/\r\n/)[1].trim()
+                    }
+                });
+            });
+
+            return results;
+        };
+    });
+}());
