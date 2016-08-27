ddc576166e7c040511318c58f8b168ab20174d51
Tomas Korcak <korczis@gmail.com>
2016-08-27 20:09:28 +0200

    Persistent data

--- docks/elasticsearch/run.sh
+++ docks/elasticsearch/run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-docker run -d -p 9200:9200 -p 9300:9300 --name elasticsearch elasticsearch
+docker run -d -p 9200:9200 -p 9300:9300 -v "/data/elasticsearch":/usr/share/elasticsearch/data --name elasticsearch elasticsearch
