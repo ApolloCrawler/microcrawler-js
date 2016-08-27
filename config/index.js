b37a5413d09fd776fdd9d9774113a5962970278d
Tomas Korcak <korczis@gmail.com>
2016-08-27 14:41:22 +0200

    Add config

---
+++ config/index.js
@@ -0,0 +1,30 @@
+export default {
+  amqp: {
+    uri: 'amqp://korczis.com',
+    queues: {
+      collector: 'collector',
+      worker: 'worker'
+    }
+  },
+
+  couchbase: {
+    uri: 'couchbase://korczis.com:8091',
+
+    bucket: 'microcrawler',
+    username: 'Administrator',
+    password: 'Administrator',
+
+    // NOTE: Times are in usec
+    connectionTimeout: 60 * 1000 * 1000,
+    durabilityTimeout: 60 * 1000 * 1000,
+    managementTimeout: 60 * 1000 * 1000,
+    nodeConnectionTimeout: 10 * 1000 * 1000,
+    operationTimeout: 10 * 1000 * 1000,
+    viewTimeout: 10 * 1000 * 1000
+  },
+
+  elastic: {
+    host: 'localhost',
+    port: 9200
+  }
+}
