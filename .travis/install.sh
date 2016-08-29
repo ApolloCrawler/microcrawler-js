ebf6a6c03dfc9f987c9cb9cc83cad831e13f91e6
Tomas Korcak <korczis@gmail.com>
2016-08-29 19:42:21 +0200

    Swap linux and osx install commands

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  brew update # get list of latest packages
+  brew install libcouchbase
+else
   # Only needed during first-time setup:
   wget http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-2-amd64.deb
   sudo dpkg -i couchbase-release-1.0-2-amd64.deb
@@ -8,7 +11,4 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   # Will install or upgrade packages
   sudo apt-get update
   sudo apt-get install libcouchbase-dev libcouchbase2-bin build-essential
-else
-  brew update # get list of latest packages
-  brew install libcouchbase
 fi
